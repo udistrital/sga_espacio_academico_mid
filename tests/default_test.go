ade3abd503f22e625873eea539f775064b83d2f2
Moreraz20 <felipemorera15@gmail.com>
2024-02-25 16:55:41 -0500

    Refactor: udistrital/sga_documentacion#67 sga_espacio_academico_mid actualizacion rutas

--- tests/default_test.go
+++ tests/default_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 	"runtime"
 	"path/filepath"
-	_ "sga_mid_espacios_academicos/routers"
+	_ "github.com/udistrital/sga_espacio_academico_mid/routers"
 
 	"github.com/astaxie/beego"
 	. "github.com/smartystreets/goconvey/convey"
